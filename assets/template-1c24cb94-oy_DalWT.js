0d1488254ec7ae76b43431c9e6383686568b2a68
ia.burcev <ia.burcev@tensor.ru>
2024-09-15 22:24:07 +0300

    Updates

---
+++ assets/template-1c24cb94-oy_DalWT.js
@@ -0,0 +1,1 @@
+import{A as a,E as t}from"./index-CPSEdcNq.js";var s=function(o){var l=o.id,e=o.modal,i=e===void 0||e;return a.createElement("div",{onClick:function(){i&&t.dialogClose(l)},className:"DialogModal-wrapper "+o.wrapperClassName},a.createElement("div",{onClick:function(r){r.stopPropagation()},style:o.styles,className:"DialogModal-body"},o.children))},d=a.memo(s);export{d as default};
